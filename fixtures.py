259152b742c7c5d551186fcb220a82eba7883b49
Федоров Данила Вячеславович <d.fedorov@transtelematica.ru>
2026-01-08 19:02:44 +0300

    add ci/cd github

--- fixtures.py
+++ fixtures.py
@@ -1,35 +1,22 @@
-# fixtures.py - ПОЛНАЯ версия для Google Colab
+# conftest.py - фикстуры для GitHub Actions
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 
+@pytest.fixture(scope="session")
 def chrome_driver():
-    """Chrome ИДЕАЛЬНО для Google Colab"""
+    """Chrome для GitHub Actions"""
     options = Options()
-
-    # 🔥 ОСНОВНЫЕ флаги Colab
-    options.add_argument('--headless')
+    options.add_argument('--headless=new')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
-
-    # 🔥 ФИКС DevToolsActivePort
+    options.add_argument('--window-size=1920,1080')
     options.add_argument('--disable-extensions')
-    options.add_argument('--disable-background-timer-throttling')
-    options.add_argument('--disable-backgrounding')
-    options.add_argument('--disable-renderer-backgrounding')
-    options.add_argument('--disable-features=TranslateUI')
-    options.add_argument('--disable-ipc-flooding-protection')
 
-    # 🔥 СТАБИЛЬНОСТЬ
-    options.add_argument('--remote-debugging-port=9222')
-    options.add_argument('--disable-web-security')
-    options.add_argument('--allow-running-insecure-content')
-
-    # ПУТИ Colab
     service = Service('/usr/bin/chromedriver')
-    options.binary_location = '/usr/bin/chromium-browser'
-
     driver = webdriver.Chrome(service=service, options=options)
-    return driver
+    yield driver
+    driver.quit()
